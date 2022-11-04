71b85031d1aa8cb31768d40a217bc9799ecc1463
Johnny <johnnyturco@me.com>
2022-11-04 16:15:25 -0600

    commit tree 7230035b586c62560227c719de021dabb7dfed4e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName, userEmail){
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  })
+  .then(response => response.json)
+  .then(data => {
+    document.body.innerHTML = data["id"]
+    // const body = document.querySelector("body");
+    // const newDiv = document.createElement("div");
+    // newDiv.textContent = data["id"];
+    // body.prepend(newDiv);
+  })
+};
